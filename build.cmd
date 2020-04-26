4132f5b37b32a8bd2852bcd5369fb9ec90afa97e
kkbruce <bruce.mvc@outlook.com>
2020-04-26 22:43:57 +0800

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-docker build -t kkbruce/redis-master:4.0.14.2-aof-nanoserver-1909 -f .\Dockerfile-Master .
-docker build -t kkbruce/redis-slave:4.0.14.2-aof-nanoserver-1909 -f .\Dockerfile-Slave .
-docker build -t kkbruce/redis-sentinel:4.0.14.2-aof-nanoserver-1909 -f .\Dockerfile-Sentinel .
+docker build -t kkbruce/redis:master-4.0.14.2-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Master .
+docker build -t kkbruce/redis:slave-4.0.14.2-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Slave .
+docker build -t kkbruce/redis:sentinel-4.0.14.2-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Sentinel .
