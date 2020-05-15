a8c79d7d1b006d2981355c9b43f8854415ba766e
kkbruce <bruce.mvc@outlook.com>
2020-05-15 16:58:22 +0800

    upgrade to redis 5.0.9

--- build.cmd
+++ build.cmd
@@ -1,3 +1,7 @@
-docker build -t kkbruce/redis:master-4.0.14.2-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Master .
-docker build -t kkbruce/redis:slave-4.0.14.2-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Slave .
-docker build -t kkbruce/redis:sentinel-4.0.14.2-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Sentinel .
+docker build -t kkbruce/redis:master-5.0.9-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Master .
+docker build -t kkbruce/redis:slave-5.0.9-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Slave .
+docker build -t kkbruce/redis:sentinel-5.0.9-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Sentinel .
+
+docker push kkbruce/redis:master-5.0.9-aof-nanoserver-1909
+docker push kkbruce/redis:slave-5.0.9-aof-nanoserver-1909
+docker push kkbruce/redis:sentinel-5.0.9-aof-nanoserver-1909
