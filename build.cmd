f33caa18b62eec4f19c04214d8e74b7b2a541bc4
kkbruce <bruce.mvc@outlook.com>
2020-08-23 11:37:10 +0800

    update Server Core 2004

--- build.cmd
+++ build.cmd
@@ -1,7 +1,15 @@
-docker build -t kkbruce/redis:master-5.0.9-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Master .
-docker build -t kkbruce/redis:slave-5.0.9-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Slave .
-docker build -t kkbruce/redis:sentinel-5.0.9-aof-nanoserver-1909 --network "Default Switch" -f .\Dockerfile-Sentinel .
+docker build -t kkbruce/redis:master-4.0.14.2-aof-nanoserver-2004 --network "Default Switch" -f .\Dockerfile-Master .
+docker build -t kkbruce/redis:slave-4.0.14.2-aof-nanoserver-2004 --network "Default Switch" -f .\Dockerfile-Slave .
+docker build -t kkbruce/redis:sentinel-4.0.14.2-aof-nanoserver-2004 --network "Default Switch" -f .\Dockerfile-Sentinel .
 
-docker push kkbruce/redis:master-5.0.9-aof-nanoserver-1909
-docker push kkbruce/redis:slave-5.0.9-aof-nanoserver-1909
-docker push kkbruce/redis:sentinel-5.0.9-aof-nanoserver-1909
+docker push kkbruce/redis:master-4.0.14.2-aof-nanoserver-2004
+docker push kkbruce/redis:slave-4.0.14.2-aof-nanoserver-2004
+docker push kkbruce/redis:sentinel-4.0.14.2-aof-nanoserver-2004
+
+REM docker build -t kkbruce/redis:master-5.0.9-aof-nanoserver-2004 --network "Default Switch" -f .\Dockerfile-Master .
+REM docker build -t kkbruce/redis:slave-5.0.9-aof-nanoserver-2004 --network "Default Switch" -f .\Dockerfile-Slave .
+REM docker build -t kkbruce/redis:sentinel-5.0.9-aof-nanoserver-2004 --network "Default Switch" -f .\Dockerfile-Sentinel .
+
+REM docker push kkbruce/redis:master-5.0.9-aof-nanoserver-2004
+REM docker push kkbruce/redis:slave-5.0.9-aof-nanoserver-2004
+REM docker push kkbruce/redis:sentinel-5.0.9-aof-nanoserver-2004
